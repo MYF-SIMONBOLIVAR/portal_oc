6700787089f1bacbed05b89c5109586e45c37a12
MUELLES Y FRENOS SIMON BOLIVAR <tic3@repuestossimonbolivar.com>
2026-02-02 12:13:21 -0500

    Update trpc.ts

--- trpc.ts
+++ trpc.ts
@@ -1,4 +1,4 @@
 import { createTRPCReact } from "@trpc/react-query";
-import type { AppRouter } from "./index"; // Asegúrate de que apunte a tu archivo donde está el AppRouter
+import type { AppRouter } from "./index"; 
 
 export const trpc = createTRPCReact<AppRouter>();
